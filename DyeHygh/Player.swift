246f62f7488f11ba2d89586b9fcffb803075bd3f
MattyJ10 <jasaitismatt@yahoo.com>
2017-04-14 14:40:56 -0700

    starting data classes and matchmaking

---
+++ DyeHygh/Player.swift
@@ -0,0 +1,17 @@
+//
+//  Player.swift
+//  DyeHygh
+//
+//  Created by Matt Jasaitis on 4/14/17.
+//  Copyright © 2017 Jasaitis. All rights reserved.
+//
+
+import Foundation
+
+class Player {
+    
+    var name : String = ""
+    
+    
+    
+}
