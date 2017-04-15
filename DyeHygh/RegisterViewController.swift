89faf9e4153fae52e775972247cd35cf55595ecd
MattyJ10 <jasaitismatt@yahoo.com>
2017-04-15 15:46:15 -0700

    reg

---
+++ DyeHygh/RegisterViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterViewController.swift
+//  DyeHygh
+//
+//  Created by Matt Jasaitis on 4/15/17.
+//  Copyright © 2017 Jasaitis. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+}
